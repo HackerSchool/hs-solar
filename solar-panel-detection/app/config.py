6a7131e535fa1b3eb4ab826ee6f7395c464034dc
gazev <goncalo.r.azevedo@tecnico.ulisboa.pt>
2025-03-28 11:12:48 +0000

    fix: return prediction results

--- solar-panel-detection/app/config.py
+++ solar-panel-detection/app/config.py
@@ -2,4 +2,4 @@ class AppConfig:
     STATIC_URL_PATH = ""
     STATIC_FOLDER = "static/"
     TEMPLATE_FOLDER = "templates/"
-    YOLO_PATH = "../best.torchscript"
+    YOLO_PATH = "best.torchscript"
